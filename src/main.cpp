cfb51b35c72bce7ac28c06a29a90fda4cd92e51a
Heitor <heitorventura21@gmail.com>
2021-10-22 16:17:54 -0300

    implementing cgne

--- src/main.cpp
+++ src/main.cpp
@@ -117,6 +117,29 @@ Eigen::VectorXd cgnr(const Eigen::VectorXd& g, const Eigen::MatrixXd& H) {
   return f;
 }
 
+Eigen::VectorXd cgne(const Eigen::VectorXd& g, const Eigen::MatrixXd& H) {
+  Eigen::VectorXd f = Eigen::VectorXd::Zero(H.cols());
+  Eigen::VectorXd r = g - H * f;
+  Eigen::VectorXd p = H.transpose() * r;
+  double r_old_norm = r.norm();
+  for (int i = 0; i < g.size(); i++) {
+    double alpha_num = r.transpose() * r;
+    double alpha_den = p.transpose() * p;
+    double alpha = alpha_num / alpha_den;
+    r = r - alpha * H * p;
+    double error = std::abs(r.norm() - r_old_norm);
+    if (error < 1e-4) break;
+    f = f + alpha * p;
+    double beta_num = r.transpose() * r;
+    double& beta_den = alpha_num;
+    double beta = beta_num / beta_den;
+    p = H.transpose() * r + beta * p;
+    r_old_norm = r.norm();
+  }
+
+  return f;
+}
+
 std::function<void()> time_it() {
   auto start = std::chrono::system_clock::now();
   return [start]() {
@@ -149,7 +172,7 @@ int main() {
 
   std::cout << "Computing CGNR:" << std::endl;
   auto finished5 = time_it();
-  Eigen::VectorXd f = cgnr(g, H);
+  Eigen::VectorXd f = cgne(g, H);
   finished5();
   std::cout << "Done!" << std::endl;
 
